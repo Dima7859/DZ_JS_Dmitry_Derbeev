17f451f15f9ae2049a8f37cc0d44e362f96b4fd5
Dima7859 <68110092+Dima7859@users.noreply.github.com>
2021-07-09 13:19:46 +0300

    DZJS_7

--- src/shared/ls-service.js
+++
@@ -1,9 +0,0 @@
-export const getToken = () => localStorage.getItem('token');
-export const setToken = token => localStorage.setItem('token', token);
-export const removeToken = () => localStorage.removeItem('token');
-export const getUserEmail = () => localStorage.getItem('userEmail');
-export const setUserEmail = email => localStorage.setItem('userEmail', email);
-export const removeUserEmail = () => localStorage.removeItem('userEmail');
-export const getUserName = () => localStorage.getItem('userName');
-export const setUserName = userName => localStorage.setItem('userName', userName);
-export const removeUserName = () => localStorage.removeItem('userName');
